6675eebbe797a5f7290094857f3705faaa051c4d
shivaprasad.i <shivuindaragi2500@gmail.com>
2025-05-24 14:13:33 +0530

    Few usefull tab config changes

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -7,3 +7,11 @@ vim.opt.expandtab = true
 vim.o.scrollopt = 'ver,jump,hor'
 vim.o.sidescrolloff = 5
 
+local modes = {'n', 'v'}
+
+vim.keymap.set(modes, '<leader>y', '"+y', { noremap = true, silent = true })
+vim.keymap.set(modes, '<leader>p', '"+p', { noremap = true, silent = true })
+vim.keymap.set(modes, '<Tab>', ':tabnext<CR>', { noremap = true, silent = true })
+vim.keymap.set(modes, '<S-Tab>', ':tabprevious<CR>', { noremap = true, silent = true })
+vim.keymap.set(modes, '<leader>t', ':tabnew<CR>', { noremap = true, silent = true })
+vim.keymap.set(modes, '<leader>x', ':q!<CR>', { noremap = true, silent = true })
