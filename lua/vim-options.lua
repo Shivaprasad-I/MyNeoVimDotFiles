38481a119b39f83a9aaa45d4dc55915959b06556
shivaprasad.i <shivuindaragi2500@gmail.com>
2025-08-06 21:17:24 +0530

    Added sorting for suggessions and other shortcut improvements

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -8,14 +8,29 @@ vim.o.scrollopt = 'ver,jump,hor';
 vim.o.sidescrolloff = 5;
 vim.o.ignorecase = true;
 vim.o.smartcase = true;
+vim.opt.splitright = true;
 
-local modes = {'n', 'v'}
+local modes = {'n', 'v'};
+local options = { noremap = true, silent = true };
 
-vim.keymap.set(modes, '<leader>y', '"+y', { noremap = true, silent = true })
-vim.keymap.set(modes, '<leader>p', '"+p', { noremap = true, silent = true })
+-- vim.keymap.set(modes, '<leader>y', '"+y', )
+vim.keymap.set(modes, '<leader>p', '"+p', options)
 -- below is only for VSCode like setup I'll be shifting to harpoon or snipe in future
 -- as remapping tab will break <C-i>
-vim.keymap.set(modes, '<leader>l', ':tabnext<CR>', { noremap = true, silent = true })
-vim.keymap.set(modes, '<leader>h', ':tabprevious<CR>', { noremap = true, silent = true })
-vim.keymap.set(modes, '<leader>t', ':tabnew<CR>', { noremap = true, silent = true })
-vim.keymap.set(modes, '<leader>x', ':q!<CR>', { noremap = true, silent = true })
+vim.keymap.set(modes, '<leader>l', ':tabnext<CR>', options)
+vim.keymap.set(modes, '<leader>h', ':tabprevious<CR>', options)
+vim.keymap.set(modes, '<leader>t', ':tabnew<CR>', options)
+vim.keymap.set(modes, '<leader>x', ':q!<CR>', options)
+vim.keymap.set(modes, '<Esc>', "<cmd>nohlsearch<CR>", options)
+vim.keymap.set("x", "<leader>r", function()
+  -- Yank the visual selection
+  vim.cmd('normal! "vy')
+
+  -- Escape the yanked text for literal search (\V for very nomagic)
+  local escaped = vim.fn.escape(vim.fn.getreg("v"), [[\/]])
+  local cmd = string.format([[%%s/\V%s/]], escaped)
+
+  -- Open the command-line with the substitution pre-filled
+  vim.api.nvim_feedkeys(":" .. cmd, "n", false)
+end, { desc = "Search and replace visual selection with confirmation" })
+
