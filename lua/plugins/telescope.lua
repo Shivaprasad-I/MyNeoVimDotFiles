dce399e33852a11a3a96df308bf81a794027dc59
Shivaprasad Indaragi <shivaprasad.i@milestoneinternet.com>
2025-11-16 09:37:21 +0530

    Removed telescope fzf and using fzf lua as main file picker

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -26,11 +26,11 @@ return
         dependencies = {
             "nvim-lua/plenary.nvim",
             "nvim-telescope/telescope-ui-select.nvim",
-            {
-                "nvim-telescope/telescope-fzf-native.nvim",
-                build = "make",
-                cond = vim.fn.executable("make") == 1,
-            },
+            -- {
+            --     "nvim-telescope/telescope-fzf-native.nvim",
+            --     build = "make",
+            --     cond = vim.fn.executable("make") == 1,
+            -- },
         },
         config = function()
             local builtIn = require("telescope.builtin")
@@ -60,7 +60,7 @@ return
                 },
             })
             telescope.load_extension("ui-select")
-            telescope.load_extension("fzf")
+            -- telescope.load_extension("fzf")
         end
     },
     {
