13ece57f56ba3c5a38f1f884558f418f7d06e1ea
shivu <shivuindaragi2500@gmail.com>
2025-12-13 08:59:53 +0530

    removed dependency on telescope and added get methods (gm) updated packages

--- lua/core/lsp.lua
+++ lua/core/lsp.lua
@@ -12,11 +12,20 @@ vim.diagnostic.config({
         source = true
     }
 })
-local telescope = require("telescope.builtin")
+local ui = require("fzf-lua")
 vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
-vim.keymap.set("n", "gi", telescope.lsp_implementations, {})
-vim.keymap.set("n", "gd", telescope.lsp_definitions, {})
+vim.keymap.set("n", "gi", ui.lsp_implementations, {})
+vim.keymap.set("n", "gd", ui.lsp_definitions, {})
 vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
-vim.keymap.set({ "n", "v" }, "gr", telescope.lsp_references, {})
+vim.keymap.set({ "n", "v" }, "gr", ui.lsp_references, {})
 vim.keymap.set('n', '<leader>en', "<cmd>lua vim.diagnostic.goto_next({ severity = vim.diagnostic.severity.ERROR })<CR>", { noremap = true, silent = true })
 vim.keymap.set('n', '<leader>ep', "<cmd>lua vim.diagnostic.goto_prev({ severity = vim.diagnostic.severity.ERROR })<CR>", { noremap = true, silent = true })
+
+-- below will change with change in the UI provider
+vim.keymap.set('n', "gm", function()
+    ui.lsp_document_symbols({
+        regex_filter = function(item)
+            return item.kind == "Method"
+        end
+    })
+end, { desc = "Get Methods list" })
---
+++ lua/plugins/fzf-lua.lua
@@ -0,0 +1,17 @@
+return 
+{
+    {
+        "ibhagwan/fzf-lua",
+        dependencies = { "nvim-tree/nvim-web-devicons" },
+        config = function()
+            local fzf = require("fzf-lua")
+            fzf.setup({
+                files = {
+                    previewer = false
+                }
+            })
+            vim.keymap.set('n', '<C-p>', fzf.files, {})
+            vim.keymap.set('n', '<leader>f', fzf.live_grep, {})
+        end
+    }
+} 
