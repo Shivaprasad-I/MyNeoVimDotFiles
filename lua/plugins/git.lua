66840435cd3927d408d00636ad030841adb40d77
shivaprasad.i <shivuindaragi2500@gmail.com>
2025-12-18 01:51:12 +0530

    Added Neogit for git management is is similar to magit in emacs

--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -1,23 +1,20 @@
 return {
   {
-    "tpope/vim-fugitive",
-  },
-  {
     "sindrets/diffview.nvim",
     dependencies = { "nvim-lua/plenary.nvim" },
     -- cmd = { "DiffviewOpen", "DiffviewClose", "DiffviewFileHistory" },
     config = function()
       local lib = require("diffview.lib")
 
-      local function toggle_diffview()
-        if lib.get_current_view() then
-          vim.cmd("DiffviewClose")
-        else
-          vim.cmd("DiffviewOpen")
-        end
-      end
-
-      vim.keymap.set('n', '<C-g>', toggle_diffview, { noremap = true, silent = true, desc = 'Toggle Diffview' })
+      -- local function toggle_diffview()
+      --   if lib.get_current_view() then
+      --     vim.cmd("DiffviewClose")
+      --   else
+      --     vim.cmd("DiffviewOpen")
+      --   end
+      -- end
+      --
+      -- vim.keymap.set('n', '<C-g>', toggle_diffview, { noremap = true, silent = true, desc = 'Toggle Diffview' })
 
     end,
   },
@@ -43,5 +40,19 @@ return {
       vim.keymap.set({ 'n', 'v' }, '<leader>gr', gitsigns.reset_hunk, { noremap = true, silent = true })
     end
   },
+  {
+      "TimUntersberger/neogit",
+      dependencies = { "nvim-lua/plenary.nvim", "sindrets/diffview.nvim" },
+      config = function()
+          local neogit = require("neogit"); 
+          neogit.setup({
+              -- your options here, or leave empty for defaults
+              integrations = {
+                  diffview = true -- if you want diffview integration
+              }
+          })
+          vim.keymap.set('n', '<C-g>', function() neogit.open() end, { noremap = true, silent = true, desc = 'Toggle Diffview' });
+      end,
+  }
 }
 
--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -11,5 +11,11 @@ return {
         vim.api.nvim_set_hl(0, "CursorLineNr", { fg = "#ffa13d"})  -- Bold, bright color for current line number
         vim.api.nvim_set_hl(0, "Normal", { bg = "none" })  
         vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })  
+
+        -- only for diff view to add more contrast to the lines
+        vim.api.nvim_set_hl(0, "DiffAdd", { bg = "#1d2b21", fg = "#a9b665" })
+        vim.api.nvim_set_hl(0, "DiffDelete", { bg = "#2b1d1d", fg = "#ea6962" })
+        vim.api.nvim_set_hl(0, "DiffChange", { bg = "#1d1d2b", fg = "#d8a657" })
+        vim.api.nvim_set_hl(0, "DiffText", { bg = "#44475a", fg = "#ffffff" })
     end
 }
