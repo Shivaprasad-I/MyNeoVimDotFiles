488b68b19e653d93eaa0a2d846c8be7036401e9c
shivu <shivuindaragi2500@gmail.com>
2026-04-13 09:23:54 +0530

    changed config to use remote repo

--- lua/plugins/custom-functions.lua
+++ lua/plugins/custom-functions.lua
@@ -1,8 +1,8 @@
 return {
-    "your-username/custom-tools.nvim",
+    "shivaprasad-i/custom-functions.nvim",
     -- Use 'dir' for local development. 
     -- When you host it on GitHub, you can comment this out to use the remote version.
-    dir = "~/custom-tool.nvim",
+    -- dir = "~/custom-tool.nvim",
     config = function()
         require("custom-tools").setup()
     end,
